2042bfbb2ad6a79ddaa8413c33d43678de509515
Callum Welsh <callumwelsh@Callums-MacBook-Pro-2.local>
2025-01-18 16:10:48 -0500

    Added heat map to trajectory maker

--- generateCSVTrajectories.py
+++ generateCSVTrajectories.py
@@ -6,6 +6,26 @@ from matplotlib.path import Path
 from itertools import chain
 import csv
 import math
+import cv2
+
+
+#################
+#Heat map plotting functions
+#
+def read_grid(file_name):
+    return np.load(file_name)
+
+def normalize_to_grayscale(arr):
+    return arr/np.max(arr)*255
+
+def grayscale_to_heatmap(grayscale_array):
+    # Ensure the array is in the range [0, 255]
+    grayscale_array = np.uint8(grayscale_array)  # Convert to 8-bit unsigned integer
+
+    # Apply a color map to the grayscale array
+    heatmap = cv2.applyColorMap(grayscale_array, cv2.COLORMAP_HOT)  # You can choose any color map (e.g., COLORMAP_HOT, COLORMAP_COOL, etc.)
+
+    return heatmap
 
 def distance(point1, point2):
     # Calculate the Euclidean distance between two points using only x and y values
@@ -40,11 +60,17 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
 
 # Circle parameters
 circle_radius = 240
 circle_thickness = 10
 circle_center = (width // 2, height // 2)
+
+def convert_plot_for_CV(grid):
+    colormap_arr = grayscale_to_heatmap(normalize_to_grayscale(grid))
+    heatmap = cv2.resize(colormap_arr, (2*circle_radius,2*circle_radius), interpolation=cv2.INTER_NEAREST)
+    return heatmap
 
 # Selected points for polygon
 selected_points = []
@@ -83,9 +109,23 @@ k_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((width - 130, 
 print_coords_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((width - 230, 50), (200, 30)),
                                                    text='Save Points', manager=manager)
 
+mot_counts = read_grid("MOTLog.npy")
+shot_counts = read_grid("shotLog.npy")
+
+mean_counts = mot_counts/shot_counts
+
+mot_heatmap_cv =  cv2.cvtColor(convert_plot_for_CV(mean_counts),cv2.COLOR_BGR2RGB)
+mot_heatmap_pg = pygame.image.frombuffer(mot_heatmap_cv.tostring(), mot_heatmap_cv.shape[1::-1], 'RGB')
+
+shot_heatmap_cv =  cv2.cvtColor(convert_plot_for_CV(shot_counts),cv2.COLOR_BGR2RGB)
+shot_heatmap_pg = pygame.image.frombuffer(shot_heatmap_cv.tostring(), shot_heatmap_cv.shape[1::-1], 'RGB')
+
+mot_heatmap_mode = False
+shot_heatmap_mode = False
 # Main loop
 running = True
 while running:
+
     time_delta = pygame.time.Clock().tick(60) / 1000.0
 
     for event in pygame.event.get():
@@ -99,7 +139,7 @@ while running:
                 mouse_x, mouse_y = event.pos
                 # Check if mouse click is inside the circle
                 distance_from_center = ((mouse_x - circle_center[0]) ** 2 + (mouse_y - circle_center[1]) ** 2) ** 0.5
-                if distance_from_center <= circle_radius:
+                if distance_from_center <= circle_radius+100 and mouse_y > height/2 - 270:
                     # Add clicked point to the list
                     selected_points.append((mouse_x, mouse_y))
                     drawing_polygon = True
@@ -177,6 +217,18 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 selected_points = []  # Clear all selected points
+            if event.key == pygame.K_h:
+                if mot_heatmap_mode:
+                    mot_heatmap_mode = False
+                else:
+                    shot_heatmap_mode = False
+                    mot_heatmap_mode = True
+            if event.key == pygame.K_j:
+                if shot_heatmap_mode:
+                    shot_heatmap_mode = False
+                else:
+                    mot_heatmap_mode = False
+                    shot_heatmap_mode = True
 
         manager.process_events(event)
 
@@ -184,18 +236,21 @@ while running:
 
     # Clear the screen
     screen.fill(BLACK)
-
     # Draw the circle
     pygame.draw.circle(screen, WHITE, circle_center, circle_radius + circle_thickness)
-    pygame.draw.circle(screen, BLACK, circle_center, circle_radius)
+    pygame.draw.circle(screen, (0, 0, 0, 0), circle_center, circle_radius)
+    if mot_heatmap_mode:
+        screen.blit(mot_heatmap_pg, (circle_center[0] - circle_radius, circle_center[1] - circle_radius))
+    if shot_heatmap_mode:
+        screen.blit(shot_heatmap_pg, (circle_center[0] - circle_radius, circle_center[1] - circle_radius))
 
     # Draw selected points
     for point in selected_points:
-        pygame.draw.circle(screen, RED, point, 5)
+        pygame.draw.circle(screen, CYAN, point, 5)
 
     # Draw polygon if we have enough points and the draw_polygon toggle is active
     if len(selected_points) >= 2 and draw_polygon:
-        pygame.draw.lines(screen, RED, True, selected_points, 2)
+        pygame.draw.lines(screen, CYAN, True, selected_points, 2)
 
     # Draw GUI
     manager.draw_ui(screen)
